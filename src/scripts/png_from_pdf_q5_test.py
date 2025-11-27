a2611bfe9b00672d5e910728bdc7b000272d1c5c
gitMorelli <andrea.morels14@gmail.com>
2025-11-27 02:07:14 +0100

    other commit

--- src/scripts/png_from_pdf_q5_test.py
+++ src/scripts/png_from_pdf_q5_test.py
@@ -5,6 +5,7 @@ import os
 from src.utils.convert_utils import pdf_to_images,save_as_is,extract_images
 from src.utils.file_utils import list_files_with_extension, get_basename, create_folder,list_subfolders,get_page_number
 from time import perf_counter
+from src.utils.logging import FileWriter
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,6 +35,11 @@ def parse_args():
         action="store_true",
         help="Enable debug logging",
     )
+    parser.add_argument(
+        "--save_debug_times",
+        action="store_true",
+        help="Enlarge censor boxes before applying censorship",
+    )
     return parser.parse_args()
 
 
@@ -45,6 +51,7 @@ def main():
 
     templates_path = args.folder_path
     save_path = args.save_path
+    save_debug_times = args.save_debug_times
     subject_paths = list_subfolders(templates_path)
     subject_id = [get_basename(s) for s in subject_paths]
 
@@ -52,13 +59,18 @@ def main():
         pdf_files = list_files_with_extension(subject, "pdf", recursive=False)#get the files in the folder (they are the files for doc_5)
         n_template=5
         id=subject_id[i]
-        process_pdf_files(n_template,pdf_files,save_path,id)
+
+        log_path=os.path.join(TEMPLATES_PATH,f"{id}")
+        subj_time_logger=FileWriter(save_debug_times,
+                                        os.path.join(log_path,f"time_logger.txt"))
+        process_pdf_files(n_template,pdf_files,save_path,id,logger=subj_time_logger)
+        subj_time_logger.call_end('complete_process',block=True)
 
     logger.info("Conversion finished")
     return 0
 
 
-def process_pdf_files(n_quest,pdf_files,save_path,id):
+def process_pdf_files(n_quest,pdf_files,save_path,id,logger=None):
     num_files=len(pdf_files)
     pdf_files.sort()
     if num_files>1: #case in which i have one file per page
@@ -68,8 +80,12 @@ def process_pdf_files(n_quest,pdf_files,save_path,id):
             doc_path = os.path.join(save_path,id,f"doc_{n_quest}")
             create_folder(doc_path, parents=True, exist_ok=True)
             out_path = os.path.join(doc_path, f"page_{n_page}")
+            logger and logger.call_start('extract_images')
             images_data,doc=extract_images(pdf_file)
+            logger and logger.call_end('extract_images')
+            logger and logger.call_start('save_images')
             save_as_is(doc,0,images_data,out_path) #i always have a single image
+            logger and logger.call_end('save_images')
         #_t1 = perf_counter()
         #print(f"time={( _t1 - _t0 ):0.6f}s")
     else: #to change, now i test on q5
---
+++ src/utils/logging.py
@@ -0,0 +1,25 @@
+from time import perf_counter, time
+
+class FileWriter:
+    def __init__(self, enabled=False,path=None):
+        self.enabled = enabled
+        self.path=path
+        self.time_dict={}
+        self.call_start('complete_process',block=True)
+    
+    def call_start(self, key,block=False):
+        if block:
+            self.write("-"*50)
+        self.time_dict[key] = perf_counter()
+    
+    def call_end(self, key,block=False):
+        self.write(f"time for process {key} = {( perf_counter()-self.time_dict[key] ):0.6f}s")
+        if block:
+            self.write("-"*50)
+
+
+    def write(self, text: str):
+        if not self.enabled:
+            return
+        with open(self.path, "a", encoding="utf-8") as f:
+            f.write(text + "\n")
